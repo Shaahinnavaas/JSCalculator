9c931f73167ee1138be8d075c692695e845dd7a9
Shaahin N <111126055+Shaahinnavaas@users.noreply.github.com>
2023-11-24 18:14:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let num1 = Number(prompt("Enter the first number: "))
+let num2 = Number(prompt("Enter the second number: "))
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let result=0
+
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+ResEl=document.getElementById("res-el");
+
+function add(){
+    result=num1+num2
+    ResEl.textContent="Sum : " +result
+    
+}
+function subtract(){
+    result=num1-num2
+    ResEl.textContent="Difference : " +result
+    
+}
+function divide(){
+    result=num1/num2
+    ResEl.textContent="Quotient : " +result
+    
+}
+function multiply(){
+    result=num1*num2
+    ResEl.textContent="Product : "+result
+    
+}
